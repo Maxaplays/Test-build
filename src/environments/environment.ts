a1dd90505343c047c91f10714368ee188bd7fe23
Rcarlos112 <rcdelatorre@outlook.com>
2020-03-27 21:28:17 -0500

    cambios prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  urlServiciosBackend: 'http://localhost:63388/api/'
+  urlServiciosBackend: 'http://38.67.136.229/EdimcaPruebasWA/api/'
 };
 
 /*
