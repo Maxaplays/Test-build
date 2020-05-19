90117e9768bf7e53cb7ac3b925f0834bc57a1841
Eduardo3098 <eduardo3.uemn@gmail.com>
2020-05-19 08:33:23 -0500

    cambio excepciones

--- src/app/services/documentos/documentos-visualizacion.service.ts
+++ src/app/services/documentos/documentos-visualizacion.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
+import {catchError} from 'rxjs/operators';
+import {Direccion} from '../direcciones/direcciones.service';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentosVisualizacionService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: 'my-auth-token'
+    })
+  };
 
   constructor( private http: HttpClient) { }
 
@@ -27,4 +37,33 @@ export class DocumentosVisualizacionService {
   public getControlCalidad(ID_CRE: string, ID_CLI: string) {
     return this.getQuery(`${ID_CRE}/${ID_CLI}/3`);
   }
+
+  public getExcepciones(ID_VAL: string) {
+    return this.getQuery(`Exepcion/${ID_VAL}`);
+  }
+
+  public postExcepcion(excepcion: Excepcion) {
+    const url = environment.urlServiciosBackend + `/DocumentosVisualizacion/NuevaExepcion`;
+    return this.http.post<Excepcion>(url, excepcion, this.httpOptions)
+      .pipe(
+        catchError(this.handleError('addSmartphone', excepcion))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
+
+export class Excepcion {
+  ID_VPOL: number;
+  USR_EXC_VAL: string;
+  OBSER_VAL_EXC_VAL: string ;
+  IDE_CRE: string;
+  Tipo: string;
 }
