f32803c67ed05ac55fc786036d4668d21975253f
Rcarlos112 <rcdelatorre@outlook.com>
2020-06-22 23:30:31 -0500

    cambios de la direccion ip de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlServiciosBackend: 'http://38.67.136.229/EdimcaPruebasWA/api/'
-  // urlServiciosBackend: 'http://38.67.136.229/edimcaproduccionbackend/api'
+  // urlServiciosBackend: 'http://38.67.136.229/edimcaproduccionbackend/api/'
 };
