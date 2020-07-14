af94898029d2adf219706ff99b0ed5e814f0c7d3
José Alberto Yonfá Célleri <yonfacasa@gmail.com>
2020-07-14 00:11:44 -0500

    Cambios Jose redireccion consulta General

--- src/app/components/content-fabrica-control-de-calidad/content-fabrica-control-de-calidad.component.ts
+++ src/app/components/content-fabrica-control-de-calidad/content-fabrica-control-de-calidad.component.ts
@@ -147,6 +147,9 @@ export class ContentFabricaControlDeCalidadComponent implements OnInit {
               this.loading = false;
               this.advertenceMessage = mensajeAdvertencia;
               this.modalService.open(contentA, {windowClass: 'custom-width-error-modal'});
+              if (mensajeAdvertencia[0] === 'S') {
+                this.router.navigate(['/fabrica/consulta-general']);
+              }
             }
             this.controlCalidad = this.getControlCalidad();
           }
