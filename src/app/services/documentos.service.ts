aaefad79611a941ab0ba0e92c484250badb95633
Eduardo3098 <eduardo3.uemn@gmail.com>
2021-01-12 20:34:11 -0500

    cambios Eduardo - Entrega de carpeta

--- src/app/services/documentos.service.ts
+++ src/app/services/documentos.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import {Telefono} from './telefonos/telefonos.service';
+import {catchError} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentosService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: 'my-auth-token'
+    })
+  };
 
   constructor(private http: HttpClient) {
     // console.log('DocumentosService');
@@ -17,13 +27,13 @@ export class DocumentosService {
     return this.http.get(url);
   }
 
-  public getEntidadFinanciera(){
+  public getEntidadFinanciera() {
     return this.getQuery('EntidadFinanciera');
   }
-  public getTipoCuenta(){
+  public getTipoCuenta() {
     return this.getQuery(`TipoCuenta`);
   }
-  public getNumeroCuenta(id_banco: string, cod_tcu : string){
+  public getNumeroCuenta(id_banco: string, cod_tcu : string) {
     return this.getQuery(`NumeroCuenta/${id_banco},${cod_tcu}`);
   }
   public getDocumentosSubidos(idCredito: string) {
@@ -39,7 +49,34 @@ export class DocumentosService {
     }
     return this.http.post<File>(url, formData);
   }
+
   public entregarCarpeta(idCredito: string, clave: string, persona: string) {
-    return this.getQuery(`entregarCarpeta/${idCredito}/${clave}/${persona}/${ localStorage.getItem('usuario')}`);
+    const carpeta: Carpeta = new Carpeta();
+    carpeta.idCredito = idCredito;
+    carpeta.clave = clave;
+    carpeta.persona = persona;
+    carpeta.usuario = localStorage.getItem('usuario');
+    const url = environment.urlServiciosBackend + 'documentos/entregarCarpeta';
+
+    return this.http.post<Carpeta>(url, carpeta, this.httpOptions)
+      .pipe(
+        catchError(this.handleError('addSmartphone', carpeta))
+      );
   }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      // console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
+
+export class Carpeta {
+  idCredito: string;
+  clave: string;
+  persona: string;
+  usuario: string;
 }
