2e4a7cef68bf605e87b168d777a656766db15900
Eduardo3098 <eduardo3.uemn@gmail.com>
2021-03-23 10:52:39 -0500

    cambios Eduardo - Historial UX

--- src/app/components/modal-historial/modal-historial.component.ts
+++ src/app/components/modal-historial/modal-historial.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Historial, HistorialService} from '../../services/historial/historial.service';
+import {map} from 'rxjs/operators';
+import {DatosFabrica, FabricaService} from '../../services/fabricaCredito/fabrica.service';
 
 @Component({
   selector: 'app-modal-historial',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalHistorialComponent implements OnInit {
 
-  constructor() { }
+  idCredito: string;
+  mensajeServicio: DatosFabrica;
+  estado: string;
+  diasEstado: number;
+  subEstado: string;
+  diasSubEstado: number;
+  comentario;
+  historial: any[] = [];
+
+  constructor( private historialService: HistorialService,
+               private fabricaService: FabricaService ) { }
 
   ngOnInit() {
+    this.fabricaService.currentMessage.subscribe(
+      data => {
+        this.mensajeServicio = data;
+        this.idCredito = this.mensajeServicio.Condicion;
+      });
+    this.getEstado();
+    this.getHistorial();
   }
 
+  public getEstado(): any {
+    this.historialService.getEstadoSubEstado(this.idCredito)
+      .pipe(map(data => data['ESTADO']))
+      .subscribe((data: any) => {
+        this.estado = data[0].ESTADO;
+        this.diasEstado = data[0].DIASESTADO;
+        this.subEstado = data[0].SUBESTADO;
+        this.diasSubEstado = data[0].DIASSUBESTADO;
+      });
+  }
+
+  public getHistorial(): any {
+    this.historialService.getHistorial(this.idCredito)
+      .pipe(map(data => data['HISTORIAL']))
+      .subscribe((data: any) => {
+        this.historial = data;
+      });
+  }
+
+  public postHistorial() {
+    if (this.comentario !== '' && this.comentario !== 'undefined') {
+      const comentario: Historial = new Historial();
+      comentario.ID_CRE = this.idCredito;
+      comentario.DES_HIST = this.comentario;
+      comentario.USR_HIST = localStorage.getItem('usuario');
+
+      this.historialService.postComentario(comentario).subscribe(
+        (data: any) => {
+          this.getHistorial();
+          this.comentario = '';
+        });
+    }
+  }
 }
---
+++ src/app/services/historial/historial.service.ts
@@ -0,0 +1,60 @@
+import {Injectable, Input} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {FabricaService} from '../fabricaCredito/fabrica.service';
+import {Observable, of} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {Direccion} from '../direcciones/direcciones.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistorialService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: 'my-auth-token'
+    })
+  };
+
+  constructor( private http: HttpClient,
+               private fabricaService: FabricaService) { }
+
+  private getQuery(query: string) {
+    const urlHistorial = 'historial/';
+    const url = environment.urlServiciosBackend + urlHistorial + query;
+    return this.http.get(url);
+  }
+
+  public getHistorial(ID_CRE: string) {
+    return this.getQuery(`${ID_CRE}`);
+  }
+
+  public getEstadoSubEstado(ID_CRE: string) {
+    return this.getQuery(`estado/${ID_CRE}`);
+  }
+
+  public postComentario(historial: Historial) {
+    const url = environment.urlServiciosBackend + `historial`;
+    return this.http.post<Direccion>(url, historial, this.httpOptions)
+      .pipe(
+        catchError(this.handleError('addSmartphone', historial))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      // console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
+
+export class Historial {
+  ID_CRE: string;
+  DES_HIST: string;
+  USR_HIST: string;
+}
